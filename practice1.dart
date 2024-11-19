59e5a4d502fc75212569d2b91a6b289a9f28b40a
Sangam Basnet <sangambasnet1800@gmail.com>
2024-11-19 09:09:13 +0545

    first

---
+++ practice1.dart
@@ -0,0 +1,6 @@
+void main() {
+  String sentence = 'My name is Sangam Baset';
+  List<String> words = sentence.split(' ');
+  int wordCount = words.length;
+  print('Number of words: $wordCount');
+}

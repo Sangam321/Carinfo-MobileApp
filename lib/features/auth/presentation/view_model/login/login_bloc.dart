a2422dacf20085b57cdb1a2bb7fdc2c92df3f7a4
Sangam Basnet <sangambasnet1800@gmail.com>
2025-02-26 13:56:29 +0545

    update in login view model

--- lib/features/auth/presentation/view_model/login/login_bloc.dart
+++ lib/features/auth/presentation/view_model/login/login_bloc.dart
@@ -1,6 +1,8 @@
+import 'package:carinfo/core/common/snackbar/my_snackbar.dart';
 import 'package:carinfo/features/auth/domain/use_case/login_usecase.dart';
 import 'package:carinfo/features/auth/presentation/view_model/signup/register_bloc.dart';
 import 'package:carinfo/features/home/presentation/view/home_view.dart';
+import 'package:carinfo/features/home/presentation/view_model/home_cubit.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,16 +11,48 @@ part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  final RegisterBloc _signupBloc;
+  final RegisterBloc _registerBloc;
+  final HomeCubit _homeCubit;
   final LoginUseCase _loginUseCase;
 
   LoginBloc({
     required RegisterBloc registerBloc,
+    required HomeCubit homeCubit,
     required LoginUseCase loginUseCase,
-  })  : _signupBloc = registerBloc,
+  })  : _registerBloc = registerBloc,
+        _homeCubit = homeCubit,
         _loginUseCase = loginUseCase,
         super(LoginState.initial()) {
-    // Event handler for logging in
+    on<NavigateRegisterScreenEvent>(
+      (event, emit) {
+        Navigator.push(
+          event.context,
+          MaterialPageRoute(
+            builder: (context) => MultiBlocProvider(
+              providers: [
+                BlocProvider.value(value: _registerBloc),
+              ],
+              child: event.destination,
+            ),
+          ),
+        );
+      },
+    );
+
+    on<NavigateHomeScreenEvent>(
+      (event, emit) {
+        Navigator.pushReplacement(
+          event.context,
+          MaterialPageRoute(
+            builder: (context) => BlocProvider.value(
+              value: _homeCubit,
+              child: event.destination,
+            ),
+          ),
+        );
+      },
+    );
+
     on<LoginUserEvent>(
       (event, emit) async {
         emit(state.copyWith(isLoading: true));
@@ -32,7 +66,6 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         result.fold(
           (failure) {
             emit(state.copyWith(isLoading: false, isSuccess: false));
-            // Show Snackbar for failure
             showMySnackBar(
               context: event.context,
               message: "Invalid Credentials",
@@ -41,58 +74,16 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
           },
           (token) {
             emit(state.copyWith(isLoading: false, isSuccess: true));
-            // Show Snackbar for success
-            showMySnackBar(
-              context: event.context,
-              message: "Login Successful",
-              color: Colors.green,
+            add(
+              NavigateHomeScreenEvent(
+                context: event.context,
+                destination: HomeView(),
+              ),
             );
-            // Dispatch a navigation event
-            add(NavigateHomeScreenEvent(
-              context: event.context,
-              destination: const HomeView(),
-            ));
+            //_homeCubit.setToken(token);
           },
         );
       },
     );
-
-    // Event handler for navigation to the home screen
-    on<NavigateHomeScreenEvent>(
-      (event, emit) {
-        Navigator.pushReplacement(
-          event.context,
-          MaterialPageRoute(
-            builder: (context) => event.destination,
-          ),
-        );
-      },
-    );
-
-    // Event handler for navigation to the registration screen
-    on<NavigateRegisterScreenEvent>(
-      (event, emit) {
-        Navigator.push(
-          event.context,
-          MaterialPageRoute(
-            builder: (context) => BlocProvider.value(
-                value: _signupBloc, child: event.destination),
-          ),
-        );
-      },
-    );
   }
-}
-
-// Snackbar utility function
-void showMySnackBar({
-  required BuildContext context,
-  required String message,
-  required Color color,
-}) {
-  final snackBar = SnackBar(
-    content: Text(message),
-    backgroundColor: color,
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }

02071c951edbb49f6bc540b985e1a17ee9803022
Sangam Basnet <sangambasnet1800@gmail.com>
2025-02-26 13:56:52 +0545

    update in api service

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -2,6 +2,7 @@ import 'package:carinfo/app/constants/api_endpoints.dart';
 import 'package:carinfo/core/network/dio_error_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // If using SharedPreferences for storing token
 
 class ApiService {
   final Dio _dio;
@@ -13,12 +14,25 @@ class ApiService {
       ..options.baseUrl = ApiEndpoints.baseUrl
       ..options.connectTimeout = ApiEndpoints.connectionTimeout
       ..options.receiveTimeout = ApiEndpoints.receiveTimeout
-      ..interceptors.add(DioErrorInterceptor())
+      ..interceptors.add(DioErrorInterceptor()) // Custom error handling
       ..interceptors.add(PrettyDioLogger(
           requestHeader: true, requestBody: true, responseHeader: true))
       ..options.headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       };
+
+    // Add an interceptor to add the Authorization header if a token exists
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final prefs = await SharedPreferences.getInstance();
+        final token = prefs.getString(
+            'token'); // Assuming the JWT is saved in shared preferences
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        return handler.next(options); // Continue request
+      },
+    ));
   }
 }

d6131470d7e3d55479a9537411eeee0b816638e1
Sangam Basnet <sangambasnet1800@gmail.com>
2025-02-26 13:54:20 +0545

    update in api

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -3,12 +3,16 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/";
+  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
 
   // ====================== Auth Routes ======================
-  static const String login = "auth/login";
-  static const String register = "auth/register";
-  static const String imageUrl = "http://10.0.2.2:3000/uploads/";
-  // static const String updateProfile = "auth/update-profile";
-  static const String uploadImage = "auth/uploadImage";
+  static const String login = "user/login";
+  static const String register = "user/register";
+  static const String logout = "user/logout";
+  static const String getUserProfile = "user/profile";
+  static const String updateProfile = "user/profile/update";
+
+  // Profile Image Upload (This is the same route as updateProfile)
+  static const String uploadImage =
+      "user/profile/update"; // Using updateProfile endpoint for image upload
 }

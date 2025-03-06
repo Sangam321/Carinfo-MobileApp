4f613f57de8da6224bf352229c6f231477525202
Sangam Basnet <sangambasnet1800@gmail.com>
2025-03-06 21:12:47 +0545

    UserEntity model to represent user profile data

---
+++ lib/features/profile/domain/entity/user_entity.dart
@@ -0,0 +1,13 @@
+class UserEntity {
+  final String name;
+  final String email;
+  final String role;
+  final String photoUrl;
+
+  UserEntity({
+    required this.name,
+    required this.email,
+    required this.role,
+    required this.photoUrl,
+  });
+}

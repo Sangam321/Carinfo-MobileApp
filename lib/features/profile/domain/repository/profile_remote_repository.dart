b73cd1fb538bbc4bdc75f58349ca9e4a7f8b0c57
Sangam Basnet <sangambasnet1800@gmail.com>
2025-03-06 21:13:03 +0545

    ProfileRemoteRepository to fetch user profile from API.

---
+++ lib/features/profile/domain/repository/profile_remote_repository.dart
@@ -0,0 +1,33 @@
+import 'package:carinfo/core/network/api_service.dart';
+import 'package:carinfo/features/profile/domain/entity/user_entity.dart';
+import 'package:carinfo/features/profile/domain/repository/profile_repository.dart';
+import 'package:dio/dio.dart';
+
+class ProfileRemoteRepository implements ProfileRepository {
+  final ApiService apiService;
+
+  ProfileRemoteRepository({required this.apiService});
+
+  @override
+  Future<UserEntity> getUserProfile() async {
+    try {
+      final Response<dynamic>? response =
+          await apiService.getRequest('/user/profile');
+
+      if (response != null && response.data != null) {
+        final data = response.data as Map<String, dynamic>;
+
+        return UserEntity(
+          name: data['name'] ?? '',
+          email: data['email'] ?? '',
+          role: data['role'] ?? '',
+          photoUrl: data['photoUrl'] ?? '',
+        );
+      } else {
+        throw Exception("No data received from the server.");
+      }
+    } catch (e) {
+      throw Exception("Failed to load user profile: ${e.toString()}");
+    }
+  }
+}
